c92f51366c94130e1a7bb59aef128a9091b18e55
Allcharmian <91695211+Allcharmian@users.noreply.github.com>
2022-06-02 02:55:44 -0500

    update frontend compras

---
+++ src/componentes/compras.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PokeData from "./card";
+const PokeCard = () => {
+  return (
+    <div>
+      {/* <h2>pokecard</h2>*/}
+      <PokeData />
+    </div>
+  );
+};
+
+export default PokeCard;
